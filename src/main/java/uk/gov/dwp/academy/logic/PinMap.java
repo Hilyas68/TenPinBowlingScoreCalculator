493b0720ebe785d06cab65835d53599eaf8ee1aa
Hassan Liasu <hassan.liasu@engineering.digital.dwp.gov.uk>
2023-09-26 15:15:02 +0100

    Given pin, confirm state

---
+++ src/main/java/uk/gov/dwp/academy/logic/PinMap.java
@@ -0,0 +1,20 @@
+package uk.gov.dwp.academy.logic;
+
+import java.util.HashMap;
+
+public class PinMap {
+  private HashMap<Integer, Integer> record;
+
+  public PinMap() {
+    record = new HashMap<>();
+  }
+
+  public void put(int pins) {
+    int size = record.size();
+    record.put(size + 1, pins);
+  }
+
+  public HashMap<Integer, Integer> get() {
+      return record;
+  };
+}
