ae46b415764ca23eb14e2c8066ee02409a9d5294
Hassan Liasu <hassan.liasu@engineering.digital.dwp.gov.uk>
2023-09-26 11:46:57 +0100

    Given an invalid pin then return outcome as false: refactor

--- src/test/java/uk/gov/dwp/academy/PlayerGameTest.java
+++ src/test/java/uk/gov/dwp/academy/PlayerGameTest.java
@@ -4,15 +4,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import uk.gov.dwp.academy.tenpin.RollResponse;
 
 public class PlayerGameTest {
 
-  @Test
+  @ParameterizedTest
+  @CsvSource({"-1","11"})
   @DisplayName("Given an invalid pin then return outcome as false")
-  public void givenInvalidPinReturnFalse(){
+  public void givenInvalidPinReturnFalse(int input){
     PlayerGame playerGame = new PlayerGame();
-    RollResponse rollResponse = playerGame.roll(-1);
+    RollResponse rollResponse = playerGame.roll(input);
     assertEquals(false, rollResponse.success(), "should return false");
   }
 
